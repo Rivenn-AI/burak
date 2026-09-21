69cd0618f5eff0c7ce5fbebd028b2939e08ee825
Rivenn-AI <jalolbekatamurodov@gmail.com>
2026-09-21 11:13:28 +0900

    Birinchi initial setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+console.log("executed");
+console.log("Hello world");
+console.log("hello mom");
+import moment from "moment";
+const currentTime = moment().format("YYYY,MM,DD");
+console.log(currentTime);
+const person: string = "riven";
+const count: number = 20;
