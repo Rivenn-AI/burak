609b029898b1f7d7da0fa466d76d0a77262d653a
Rivenn-AI <jalolbekatamurodov@gmail.com>
2026-09-23 09:23:26 +0900

    feat: Connect moongoDb with moongose

--- src/server.ts
+++ src/server.ts
@@ -10,3 +10,16 @@
 //Architectural pattern: MVC, DI, MVP
 // MVC---> Modul View Controller
 //Design patter: Midleware, Decotar
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("Connection is sucsided");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("Error occured", err));
+// console.log("PORT", process.env.PORT);
+
+// console.log("BIZNI LINK", process.env.MONGO_URL);
